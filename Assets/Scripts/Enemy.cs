9b41d3461fa7864649f1a41d41d2fb4e8d04992d
Gabriel7721m <tribui1990@gmail.com>
2026-09-25 14:21:01 +0700

    Cập nhật số lượng HP của enemy sau khi damage.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,12 @@ public class Enemy : MonoBehaviour
 {
     public string enemyType = "Goblin";
     public int damage = 25;
+    public int healthPoint = 75;
     void Start()
     {
         Debug.Log("Enemy:" + enemyType);
         Debug.Log("Damage:" + damage);
+        Debug.Log("Enemy HP:" + healthPoint);
     }
 
     void Update()
