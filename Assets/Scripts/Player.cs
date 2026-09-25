cca098e81bc2e82dfc630e535ea916024e0d835e
Gabriel7721m <tribui1990@gmail.com>
2026-09-25 13:42:57 +0700

    Gửi player 250 gold.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int level = 3;
+    public int gold = 250;
     void Start()
     {
         Debug.Log("Player started");
-        Debug.Log("Player level:" + level);
+        Debug.Log("Player level: " + level);
+        Debug.Log("Player gold: " + gold);
     }
 
     void Update()
